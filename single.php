c3a4481864a0588685ec25481f57785219bb6b14
jomurgel <dev@jomurgel.com>
2019-02-10 16:37:41 -0700

    hides comments unless the post type is writing

--- single.php
+++ single.php
@@ -20,8 +20,10 @@ get_header();
 
 				get_template_part( 'template-parts/content', 'single' );
 
+
+
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
+				if ( ( comments_open() || get_comments_number() ) && is_category( 'writing' ) ) :
 					comments_template();
 				endif;
 			endwhile; // End of the loop.
