a1c0c98252e7ab5a98641e18fd61014aa90d7398
jomurgel <dev@jomurgel.com>
2019-02-20 07:34:56 -0700

    adds favicon icons

--- header.php
+++ header.php
@@ -18,6 +18,15 @@ use function JoTheme\Functions\display_search_toggle;
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() . '/src/favicon/apple-touch-icon.png' ); ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() . '/src/favicon/favicon-32x32.png' ); ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() . '/src/favicon/favicon-16x16.png' ); ?>">
+	<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() . '/src/favicon/site.webmanifest' ); ?>">
+	<link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() . '/src/favicon/safari-pinned-tab.svg' ); ?>" color="#d7046b">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
+
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 
 	<?php display_google_analytics(); ?>
