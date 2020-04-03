dc692a440e08b792cda9e28ef4051587d6a4a3d1
jomurgel <dev@jomurgel.com>
2020-04-02 18:32:03 -0600

    updates comment

--- template-parts/content-photography.php
+++ template-parts/content-photography.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying posts
+ * Template part for displaying photography posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
