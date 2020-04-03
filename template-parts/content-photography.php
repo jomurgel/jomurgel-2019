a851b2de0ac70678ed350342ba9f5a8220924fb3
jomurgel <dev@jomurgel.com>
2020-04-02 19:32:41 -0600

    cleanup

--- template-parts/content-photography.php
+++ template-parts/content-photography.php
@@ -10,15 +10,23 @@
 
 use function JoTheme\Functions\display_post_thumbnail;
 use function JoTheme\Functions\display_posted_on;
-use function JoTheme\Functions\display_read_more;
-use function JoTheme\Functions\get_new_content;
-use function JoTheme\Functions\get_new_excerpt;
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'post-thumbnail' ); ?>>
-	<header class="post-header display-flex">
-		<span class="screen-reader-text"><?php the_title(); ?></span>
-		<?php display_post_thumbnail(); ?>
+	<header class="post-header">
+		<span class="screen-reader-text">
+			<?php the_title(); ?>
+			<div class="post-meta">
+				<?php display_posted_on(); ?>
+			</div><!-- .post-meta -->
+		</span>
+		<?php
+		display_post_thumbnail(
+			[
+				'thumbnail_linked' => 'river-photo-retina',
+			]
+		);
+		?>
 	</header><!-- .post-header -->
 </article><!-- .post -->
